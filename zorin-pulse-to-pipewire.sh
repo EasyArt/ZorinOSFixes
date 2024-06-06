fe1d1b5fb53040c54e5ca06c44270460f97c424a
Raphael Jäger <mail@jaeger-raphael.de>
2024-06-06 21:27:21 +0200

    Update zorin-pulse-to-pipewire.sh
    Added -y parameter

--- zorin-pulse-to-pipewire.sh
+++ zorin-pulse-to-pipewire.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
-sudo apt update && sudo apt install pipewire pipewire-audio-client-libraries
-sudo apt install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,media-session,bin,locales,tests}}
+sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream -y
+sudo apt update && sudo apt install pipewire pipewire-audio-client-libraries -y
+sudo apt install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,media-session,bin,locales,tests}} -y
 systemctl --user daemon-reload
 systemctl --user --now disable pulseaudio.service pulseaudio.socket
 systemctl --user --now enable pipewire pipewire-pulse
